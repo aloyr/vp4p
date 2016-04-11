f874c04e214f20e9c65739d79e431840bc5da7c2
Peter Carrero <peter.carrero@gmail.com>
2016-04-11 16:43:50 -0500

    common_setup: Remove commented out code

--- environments/dev/modules/common_setup/manifests/root_ssh.pp
+++ environments/dev/modules/common_setup/manifests/root_ssh.pp
@@ -5,17 +5,5 @@ class common_setup::root_ssh {
       type => $ssh_key_type,
       key  => $ssh_key,
     }
-    # file { '/root/.ssh':
-    #   ensure => 'directory',
-    #   owner => 'root',
-    #   mode => '700',
-    # }
-
-    # file { '/root/.ssh/authorized_keys':
-    #   ensure => 'file',
-    #   content => $ssh_key,
-    #   require => File [ '/root/.ssh' ],
-    #   mode => '644'
-    # }
   }
 }
