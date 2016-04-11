c6b2574f7c4a0ba5c68f99d95964893159381a28
Peter Carrero <peter.carrero@gmail.com>
2016-04-11 16:15:39 -0500

    common_setup: Add dnfgroup definition

--- environments/dev/modules/common_setup/manifests/init.pp
+++ environments/dev/modules/common_setup/manifests/init.pp
@@ -1,3 +1,20 @@
 class common_setup {
+  define dnfgroup($ensure = "present", $optional = false) {
+    case $ensure {
+      present,installed: {
+        $pkg_types_arg = $optional ? {
+          true => "--setopt=group_package_types=optional,default,mandatory",
+          default => "installed"
+        }
+        exec { "Installing $name yum group":
+          command => "dnf -y groupinstall $pkg_types_arg $name",
+          # unless => "yum -y groupinstall $pkg_types_arg $name --downloadonly",
+          onlyif => "echo '! dnf grouplist $name | grep -E \"^Installed\" > /dev/null' |bash",
+          timeout => 600,
+        }
+      }
+    }
+  }
   class {'common_setup::packages': }
+  dnfgroup{ 'Development Tools': }
 }
