ebbfb85aa377a3e1f2f92fdf5fc06bff6560de42
Peter Carrero <peter.carrero@gmail.com>
2016-04-11 17:41:57 -0500

    common_setup: Add filehttp definition

--- environments/dev/modules/common_setup/manifests/tools.pp
+++ environments/dev/modules/common_setup/manifests/tools.pp
@@ -1,4 +1,6 @@
 class common_setup::tools{
+
+  # install a group of packages via dnf
   define dnfgroup($ensure = "present", $optional = false) {
     case $ensure {
       present,installed: {
@@ -10,6 +12,26 @@ class common_setup::tools{
           command => "dnf -y groupinstall $pkg_types_arg $name",
           onlyif => "echo '! dnf grouplist \"$name\" | grep -E \"^Installed\" > /dev/null' |bash",
           timeout => 600,
+        }
+      }
+    }
+  }
+
+  # download a file from the internet
+  define filehttp($ensure = "present", $mode = "0755", $source = "/dev/null") {
+    case $ensure {
+      present,installed: {
+        exec { "Downloading $name":
+          command => "wget --no-check-certificate -O $name -q $source",
+          creates => $name,
+          timeout => 600,
+          require => Package[ "wget" ],
+        }
+        if $source != "/dev/null" {
+          file { $name:
+            mode => $mode,
+            require => Exec[ "Downloading $name" ],
+          }
         }
       }
     }
