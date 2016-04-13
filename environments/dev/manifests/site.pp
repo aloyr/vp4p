6b2b17387acea61748f424b71d74833d0aea3191
Peter Carrero <peter.carrero@gmail.com>
2016-04-13 18:12:59 -0500

    puppet: Add drupal class call

--- environments/dev/manifests/site.pp
+++ environments/dev/manifests/site.pp
@@ -5,8 +5,9 @@ Exec {
 node 'default' {
   class {'common_setup': }
   class {'lamp': require => Class['common_setup'] }
+  class {'drupal': }
   # $::networking[interfaces].each |String $key, Hash $value| {notice("interface: ${key}")}
-  $::sites.each |String $key, Hash $value| {notice("key: ${key} - hash: ${value}")}
+  # $::sites.each |String $key, Hash $value| {notice("key: ${key} - hash: ${value}")}
   # notice("interface data: ${::networking}")
   # notice("sites data: ${::sites}")
 }
