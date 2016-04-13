8035afdaf782d92954e6b0b3abf8060e53797932
Peter Carrero <peter.carrero@gmail.com>
2016-04-13 17:01:44 -0500

    example: Add example site config file

--- environments/dev/manifests/site.pp
+++ environments/dev/manifests/site.pp
@@ -5,5 +5,8 @@ Exec {
 node 'default' {
   class {'common_setup': }
   class {'lamp': require => Class['common_setup'] }
-  $::networking[interfaces].each |String $key, Hash $value| {notice("interface: ${key}")}
+  # $::networking[interfaces].each |String $key, Hash $value| {notice("interface: ${key}")}
+  $::sites.each |String $key, Hash $value| {notice("key: ${key} - hash: ${value}")}
+  # notice("interface data: ${::networking}")
+  # notice("sites data: ${::sites}")
 }
