ee41a5f7fbf7d30b5b85821b3b93c223bf031eca
Peter Carrero <peter.carrero@gmail.com>
2016-12-13 18:22:10 -0600

    Remove db escaping for now...

--- environments/dev/modules/drupal/manifests/database.pp
+++ environments/dev/modules/drupal/manifests/database.pp
@@ -8,19 +8,19 @@ class drupal::database {
         $site_name = $value['site_name']
 
         exec {"database_create_${site_name}":
-          command => "echo 'create database `$site_name`' | mysql",
+          command => "echo 'create database $site_name' | mysql",
           require => [Service['mariadb'], Package['mariadb']],
-          unless  => "echo 'use `$site_name`' | mysql -BN 2> /dev/null",
+          unless  => "echo 'use $site_name' | mysql -BN 2> /dev/null",
         }
 
         exec {"database_user_${site_name}":
-          command => "echo 'grant all on `$site_name`.* to `$site_name`@localhost identified by \"$site_name\"' | mysql",
+          command => "echo 'grant all on $site_name.* to $site_name@localhost identified by \"$site_name\"' | mysql",
           require => Exec["database_create_${site_name}"],
           unless  => "echo 'select user from mysql.user where host = \"localhost\" and user = \"$site_name\"' | mysql -BN | grep $site_name &> /dev/null",
         }
 
         exec {"database_user_external_${site_name}":
-          command => "echo 'grant all on `$site_name`.* to `$site_name`@`%` identified by \"$site_name\"' | mysql",
+          command => "echo 'grant all on $site_name.* to $site_name@% identified by \"$site_name\"' | mysql",
           require => Exec["database_create_${site_name}"],
           unless  => "echo 'select user from mysql.user where host = \"%\" and user = \"$site_name\"' | mysql -BN | grep $site_name &> /dev/null",
         }
